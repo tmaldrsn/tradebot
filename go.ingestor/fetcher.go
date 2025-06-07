eb03c297aeec6d1e747b741344357617fe44c3a4
tmaldrsn <tmaldrsn@gmail.com>
2025-06-07 14:48:17 -0400

    read data from static polygon io api call

--- go.ingestor/fetcher.go
+++ go.ingestor/fetcher.go
@@ -1,22 +1,62 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
 	"time"
+
+	polygon "github.com/polygon-io/client-go/rest"
+	"github.com/polygon-io/client-go/rest/models"
 )
 
 func FetchCandles(ticker string, interval string) ([]Candle, error) {
-	now := time.Now().Truncate(time.Minute)
-	return []Candle{
-		{
+	api_key := os.Getenv("POLYGON_API_KEY")
+	c := polygon.New(api_key)
+
+	from, err := time.Parse("2006-01-02", "2025-02-09")
+	if err != nil {
+		log.Fatalf("Error parsing 'from' date: %v", err)
+	}
+	to, err := time.Parse("2006-01-02", "2025-02-10")
+	if err != nil {
+		log.Fatalf("Error parsing 'to' date: %v", err)
+	}
+
+	params := models.ListAggsParams{
+		Ticker:     "X:BTCUSD",
+		Multiplier: 5,
+		Timespan:   "minute",
+		From:       models.Millis(from),
+		To:         models.Millis(to),
+	}.
+		WithAdjusted(true).
+		WithOrder(models.Order("asc")).
+		WithLimit(1000)
+
+	iter := c.ListAggs(context.Background(), params)
+
+	candles := []Candle{}
+
+	for iter.Next() {
+		result := iter.Item()
+		candles = append(candles, Candle{
 			Ticker:    ticker,
-			Timestamp: now.Unix(),
-			Open:      100.0,
-			High:      105.0,
-			Low:       98.0,
-			Close:     102.5,
-			Volume:    1200,
-			Interval:  interval,
-			Source:    "mock",
-		},
-	}, nil
+			Timestamp: time.Time(result.Timestamp).UnixMilli() / 1000, // ms → s
+			Open:      result.Open,
+			High:      result.High,
+			Low:       result.Low,
+			Close:     result.Close,
+			Volume:    result.Volume,
+			Interval:  "5m",
+			Source:    "polygon",
+		})
+	}
+
+	if err := iter.Err(); err != nil {
+		return nil, fmt.Errorf("error iterating over candles: %w", err)
+	}
+
+	return candles, nil
 }
--- go.ingestor/go.mod
+++ go.ingestor/go.mod
@@ -2,9 +2,24 @@ module github.com/tmaldrsn/tradebot/go.ingestor
 
 go 1.21
 
-require github.com/redis/go-redis/v9 v9.10.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/polygon-io/client-go v1.16.13
+	github.com/redis/go-redis/v9 v9.10.0
+)
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/form/v4 v4.2.1 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.23.0 // indirect
+	github.com/go-resty/resty/v2 v2.13.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 )
