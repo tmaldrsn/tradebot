81ffd25cfa3e2ee8aee09d2c13649d9065888156
tmaldrsn <tmaldrsn@gmail.com>
2025-07-04 22:55:05 -0400

    chore: remove golang code

--- go.ingestor/core/ingestor.go
+++
@@ -1,9 +0,0 @@
-package core
-
-import "time"
-
-type Ingestor interface {
-	FetchCandles(ticker, timeframe string, from, to time.Time) ([]Candle, error)
-	SourceName() string
-	SourceType() string
-}
