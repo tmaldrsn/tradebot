4fb9ba6dd38a71152f0d1ef22197a45ff7d3934c
tmaldrsn <tmaldrsn@gmail.com>
2025-07-11 17:10:43 -0400

    fix: use `model_dump_json()` instead of `model_dump()`

--- ingestor/src/candle/repository/redis.py
+++ ingestor/src/candle/repository/redis.py
@@ -16,7 +16,7 @@ class RedisTickerRepository:
     def save_ticker(self, ticker: TickerDTO) -> None:
         pipe = self.rdb.pipeline()
         key = REDIS_TICKER_KEY.format(ticker=ticker.abbreviation)
-        pipe.set(key, TickerDTO.model_dump())
+        pipe.set(key, ticker.model_dump_json())
         pipe.expire(key, 60*60)
         return pipe.execute()
 
