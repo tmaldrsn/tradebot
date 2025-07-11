2c85d09cee413ab6538e81de11a73b218d5af308
tmaldrsn <tmaldrsn@gmail.com>
2025-07-11 01:08:29 -0400

    feat: create `RedisCandleRepository`

--- ingestor/src/candle/repository/redis.py
+++ ingestor/src/candle/repository/redis.py
@@ -2,9 +2,11 @@ import json
 
 from redis import Redis
 
-from src.candle.models import TickerDTO
+from src.candle.models import TickerDTO, CandleDTO
 
 REDIS_TICKER_KEY = "ticker:{ticker}"
+REDIS_CANDLE_INDEX_KEY = "candle:{ticker}:{timeframe}"
+REDIS_CANDLE_KEY = "{index_key}:{timestamp}"
 
 class RedisTickerRepository:
     def __init__(self, rdb: Redis):
@@ -30,3 +32,30 @@ class RedisTickerRepository:
         raw_json = json.loads(raw)
         return TickerDTO(raw_json).model_validate()
 
+
+class RedisCandleRepository:
+    def __init__(self, rdb: Redis):
+        self.rdb = rdb
+
+
+    async def save_candles(self, candles: list[CandleDTO]) -> None:
+        pipe = self.rdb.pipeline()
+        for c in candles:
+            index_key = REDIS_CANDLE_INDEX_KEY.format(ticker=c.ticker, timeframe=c.timeframe)
+            key = REDIS_CANDLE_KEY.format(index_key=index_key, timestamp=c.timestamp)
+            pipe.set(key, json.dumps(c.model_dump_json()))
+            pipe.expire(key, 60*60)
+        await pipe.execute()
+
+
+    async def fetch_candles(self, ticker: str, timeframe: str, limit: int = 100) -> list[CandleDTO]:
+        key = f"candle:{ticker}:{timeframe}:*"
+        keys = await self.rdb.keys(key)
+        keys = sorted(keys)[-limit:]
+
+        if not keys:
+            return []
+
+        raw = self.rdb.mget(keys)
+        return [CandleDTO(c).model_validate() for c in raw if c]
+    
