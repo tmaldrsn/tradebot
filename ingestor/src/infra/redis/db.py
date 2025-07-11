408e9f9c1cd54f4077226578ca1df76ceb6ee645
tmaldrsn <tmaldrsn@gmail.com>
2025-07-11 17:08:04 -0400

    fix: update function type annotation & specific error type

--- ingestor/src/infra/redis/db.py
+++ ingestor/src/infra/redis/db.py
@@ -2,8 +2,8 @@ import os
 from redis.asyncio import Redis
 
 
-def get_redis_connection():
+def get_redis_connection() -> Redis:
     redis_host = os.getenv("REDIS_HOST")
     if not redis_host:
-        raise Exception("Environment variable `REDIS_HOST` is not set.")
+        raise ValueError("Environment variable `REDIS_HOST` is not set.")
     return Redis(host=redis_host, port=6379, decode_responses=True)
